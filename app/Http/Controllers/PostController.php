84f06ee14d9700cb863d441b2361b992e9bd4595
M-Hidayatullah <hidayatullahblk@gmail.com>
2021-08-26 14:21:46 +0800

    insert data ke database

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -21,4 +21,50 @@ class PostController extends Controller
 
         return view('post.index', compact('posts'));
     }
+
+ 
+     /**
+* create
+*
+* @return void
+*/
+public function create()
+{
+  return view('post.create');
+}
+    
+/**
+* store
+*
+* @param  mixed $request
+* @return void
+*/
+public function store(Request $request)
+{
+  $this->validate($request, [
+     'image'     => 'required|image|mimes:png,jpg,jpeg',
+     'title'     => 'required',
+     'content'   => 'required'
+  ]);
+
+  //upload image
+  $image = $request->file('image');
+  $image->storeAs('public/posts', $image->hashName());
+
+  $post = Post::create([
+      'image'     => $image->hashName(),
+      'title'     => $request->title,
+      'content'   => $request->content
+  ]);
+
+  if($post){
+    //redirect dengan pesan sukses
+    return redirect()->route('post.index')->with(['success' => 'Data Berhasil Disimpan!']);
+  }else{
+    //redirect dengan pesan error
+    return redirect()->route('post.index')->with(['error' => 'Data Gagal Disimpan!']);
+  }
+
+}
+
 }
