74567cc5c5ccd5e9abcc70bd09128edf99937445
M-Hidayatullah <hidayatullahblk@gmail.com>
2021-08-26 13:42:48 +0800

    instal serta konfigurasi tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+// webpack.mix.js
+mix.js("resources/js/app.js", "public/js")
+   .postCss("resources/css/app.css", "public/css", [
+     require("tailwindcss"),
+]);
+
+    
